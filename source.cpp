ad398a2873472f14b9cba4deeeb35721fb964bff
Ivan Windon <ivan.windon@icloud.com>
2022-02-25 07:17:51 -0500

    Editing gitignore and changing comments in source

--- source.cpp
+++ source.cpp
@@ -1,10 +1,9 @@
-//
 //  source.cpp
 //  SemesterProject - Fall 2013
 //  Professor: Dr. Steven C. Shaffer
 //  Created by Ivan Windon on 11/12/13.
-//  Copyright (c) 2013 Ivan Windon. All rights reserved.
 //  CMPSC101 - Semester Project
+
 /*  Ask the user for a list of names. Allow the user to enter up to 25 names. After all entries are received,
  sort the list and display the list in alphabetical order. You must use arrays and create a function to sort
  and a separate function to display the  array. Implement your own bubble sort (do not use one that is built in);
@@ -22,8 +21,9 @@ using namespace std;
 // Bubble Sort to sort through the array of 25 or less and then put the array into alphabetical order
 void bubbleSort(string array[], int size) {
 	bool swapped=true;
-	int count=0;  // Initialized the count to begin stepping through the array.
+	int count = 0;  // Initialized the count to begin stepping through the array.
 	int number = 1;
+
 	while(swapped) {
 		swapped=false;
 		count++;  // Increments the count by one to move to the next pass of the array.
@@ -40,10 +40,12 @@ void bubbleSort(string array[], int size) {
 }
 
 // Function that prints the names in the array onto the screen after they have been processed by the Bubble Sort
+
 void printNames(string arg[], int length) {
 	for (int n=0; n < length; n++) // Steps through the array elements one at a time.
 		cout << arg[n] << endl; // Prints the array element onto the screen.
 }
+
 int main() {
 	string names[25]; // Sets up the array to have up to 25 elements.
 	int counter = 0;  // Sets the main counter at 0 to start accepting input into the array.
@@ -58,6 +60,7 @@ int main() {
 		counter ++;  // Increments the main counter by one to input the next name into the array element.
 		counter2 ++; // Increments the display counter so you know you are entering the next number and where you are.
 	}
+
 	bubbleSort(names, counter); // Call of the Bubble Sort Function, passing the names entered as well as the number array elements.
 	cout << endl << endl << "Your names in alphabetical order: " << endl;  // Sets up display formating
     cout << setfill('-')<<setw(35)<<"-"<<endl;
